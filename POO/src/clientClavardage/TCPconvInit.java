ee34e7d3ab636baef701ec30ce9603f2160b6659
007jcs <jeanchristophesanchez@laposte.net>
2020-12-17 10:46:51 +0100

    correction bug classe tcp

---
+++ POO/src/clientClavardage/TCPconvInit.java
@@ -0,0 +1,20 @@
+package clientClavardage;
+
+public class TCPconvInit {
+	
+	
+	public static class TCPConvInit implements Runnable{
+		
+		public TCPConvInit() {
+			
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+}
